eb6d2f6c5e8398ff0894f9bcd6edea39329d338a
Jan <jandexterndiaz@gmail.com>
2026-08-17 22:47:34 +0800

    Solution: Add a fourth flavor to the menu

--- src/BookingService.java
+++ src/BookingService.java
@@ -9,6 +9,7 @@ class BookingService {
         drinks.add(new Drink("Classic Buko", "Fresh coconut water and young coconut meat", 45.00));
         drinks.add(new Drink("Buko Pandan", "Coconut juice infused with pandan", 55.00));
         drinks.add(new Drink("Buko Lychee", "Coconut juice with lychee bits", 60.00));
+        drinks.add(new Drink("Buko Melon", "Coconut juice with melon bits", 60.00));
     }
 
     List<Drink> listDrinks() {
