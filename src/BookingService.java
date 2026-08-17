44c134feaa5f4e878af7f0e8c58fb42d61a30785
Jan <jandexterndiaz@gmail.com>
2026-08-17 22:46:03 +0800

    Add BookingService with seed flavors

---
+++ src/BookingService.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class BookingService {
+
+    private final List<Drink> drinks = new ArrayList<>();
+
+    BookingService() {
+        drinks.add(new Drink("Classic Buko", "Fresh coconut water and young coconut meat", 45.00));
+        drinks.add(new Drink("Buko Pandan", "Coconut juice infused with pandan", 55.00));
+        drinks.add(new Drink("Buko Lychee", "Coconut juice with lychee bits", 60.00));
+    }
+
+    List<Drink> listDrinks() {
+        return drinks;
+    }
+
+    Drink getDrink(int index) {
+        return drinks.get(index);
+    }
+
+    String book(String customerName, Drink drink, String deliveryDate) {
+        return String.format(
+                "Booked! %s x1 %s for %s, delivering on %s.",
+                drink.name(), drink.priceLabel(), customerName, deliveryDate);
+    }
+}
